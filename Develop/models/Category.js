3ca126cc3e2c4c9e018dbf1ee68c3995b6555813
GSvenny <gabrielswagner7@gmail.com>
2022-07-27 18:40:37 -0500

    defined columns for Category.js model

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,6 +7,16 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      //Sequelize DataType objects to define what type of data it is.
+      allowNull: false,
+      //enforce columns to not accept null values
+      primaryKey: true,
+      //Creates a key that uniquely identifies each row of a table
+      autoIncrement: true
+      //each new record will automatically generate a new id value by 1
+    }
   },
   {
     sequelize,
