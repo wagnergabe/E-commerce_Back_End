18d3d0033eea701c6b1bca6522835deaa4510782
GSvenny <gabrielswagner7@gmail.com>
2022-07-27 22:30:00 -0500

    update index model, followed dev prompts

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
+//Creates a One-To-One relationship between the Procuct model and the Category model
 
 // Categories have many Products
+Category.hasMany(Product);
+//creates a one-to-many association between the Category and Product models. 
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsTo(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+  //Defines the relationship of the Procuct model to the Tag, and declare the link to the foreign key, which is designated to product_id of the Product Model
+});
 
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+})
 module.exports = {
   Product,
   Category,
