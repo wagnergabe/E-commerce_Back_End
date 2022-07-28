5246772198309d253a9e83846fb43c45edad35c8
GSvenny <gabrielswagner7@gmail.com>
2022-07-27 21:48:57 -0500

    Product model updated with requested criteria

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -15,7 +15,36 @@ Product.init(
       allownull: false,
       primarykey: true,
       autoIncrement: true
-    }
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      alllowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allownull: false,
+      validate: {
+        isDecimal: true
+        //Makes sure data has a decimal
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      //Default value is always NULL, this will keep it at 10
+      validate: {
+        isNumeric: true,
+      },
+    },
+    catagory_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+        //signals the database that this table has a reference to another. 
+      },
+    },
   },
   {
     sequelize,
@@ -27,3 +56,32 @@ Product.init(
 );
 
 module.exports = Product;
+
+
+//-checklist
+
+// --Product-- 
+// -id-
+// Integer
+// Doesn't allow null values
+// Set as primary key
+// Uses auto increment
+
+// -product_name-
+// String
+// Doesn't allow null values
+
+// -price-
+// Decimal
+// Doesn't allow null values
+// Validates that the value is a decimal
+
+// -stock-
+// Integer
+// Doesn't allow null values
+// Set a default value of 10
+// Validates that the value is numeric
+
+// -category_id-
+// Integer
+// References the category model's id
