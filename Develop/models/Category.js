6a13b84b213f5f4df9a3bafb9bba0b9b4cf2d025
GSvenny <gabrielswagner7@gmail.com>
2022-07-27 21:20:58 -0500

    updated category model

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -9,14 +9,20 @@ Category.init(
     // define columns
     id: {
       type: DataTypes.INTEGER,
-      //Sequelize DataType objects to define what type of data it is.
+      //Sequelize DataType objects to define what type of data it is. This case it will only be a number
       allowNull: false,
       //enforce columns to not accept null values
       primaryKey: true,
       //Creates a key that uniquely identifies each row of a table
       autoIncrement: true
       //each new record will automatically generate a new id value by 1
-    }
+    },
+  // define catagory name column
+  catagory_name: {
+    type: DataTypes.STRING,
+    //Datatype objects will be strings. 
+    allowNull: false
+  }
   },
   {
     sequelize,
