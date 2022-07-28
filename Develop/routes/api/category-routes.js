aeea5d3e3766a2cb1da29306061c6c4ddda74a69
GSvenny <gabrielswagner7@gmail.com>
2022-07-27 23:09:01 -0500

    update findall route for categories

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,6 +6,25 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    //Query the Category database, and then retrieves columns listed in the Product model
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock',
+          'category_id'
+        ]
+      }
+    ]
+  })
 });
 
 router.get('/:id', (req, res) => {
