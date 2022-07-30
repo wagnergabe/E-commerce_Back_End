a65ab574058876b83ecefecd1abf81355a9d5c28
GSvenny <gabrielswagner7@gmail.com>
2022-07-29 21:03:31 -0500

    create a delete route for category with product's code block

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -87,6 +87,16 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then((dbCatData) => res.json(dbCatData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
