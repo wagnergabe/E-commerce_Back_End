ce8334a7405060d216167a406a5ca8194425f418
GSvenny <gabrielswagner7@gmail.com>
2022-07-29 21:21:45 -0500

    potential tag request by id

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -28,7 +28,30 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  // be sure to include its associated Product data (Think this means what products have this tag)
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        attributes: [
+          "id",
+          "product_name",
+          "price",
+          "stock",
+          "category_id"
+      ],
+      through: ProductTag,
+      as: "products",
+      },
+    ],
+  }).then ((tagData) => res.json(tagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 router.post('/', (req, res) => {
