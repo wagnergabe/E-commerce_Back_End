48638e4b788006e30a36e8d4898eced752b2029c
GSvenny <gabrielswagner7@gmail.com>
2022-07-30 21:30:54 -0500

    fixed tag error, walkthough setup for video

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -84,7 +84,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
-  Product.destroy({
+  Tag.destroy({
     where: {
       id: req.params.id
     }
